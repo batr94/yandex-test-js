5342ac2fae91ade1b7dabb066c853a5e69832208
Batyrbek Aigaliyev <www.bat.94@gmail.com>
2020-08-28 22:27:07 +0500

    Установил и настроил babel

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  }
+};
