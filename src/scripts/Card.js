03a59560cef371ac6338d4df2567e0d1d648923a
Batyrbek Aigaliyev <www.bat.94@gmail.com>
2020-08-30 00:49:36 +0500

    Переделал класс Card для использования вместе с PopupWithImage

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,33 +1,11 @@
-const imageModalWindow = document.querySelector('.popup_type_image');
-const imageElement = imageModalWindow.querySelector('.popup__image');
-const imageCaption = imageModalWindow.querySelector('.popup__caption');
-const ESC_KEYCODE = 27;
-
-const closeModalWindow = () => {
-  imageModalWindow.classList.remove('popup_is-opened');
-  document.removeEventListener('keyup', handleEscUp);
-};
-
-const handleEscUp = (evt) => {
-  evt.preventDefault();
-  isEscEvent(evt, closeModalWindow);
-};
-
-const isEscEvent = (evt, action) => {
-  const activePopup = document.querySelector('.popup_is-opened');
-  if (evt.which === ESC_KEYCODE) {
-    action(activePopup);
-  }
-};
-
 // Эти функции и переменные -- дубли из index.js. Они нарушают DRY, но в следующем спринте студенты удалят этот код.
 // Как "Можно лучше" посоветуйте вынести эти функции и переменные в модуль utils.js и импортировать их в класс Card.
 
 class Card {
-  constructor(data, cardSelector) {
+  constructor(data, cardSelector, handleCardClick) {
     this._text = data.name;
     this._link = data.link;
-
+    this._handleShowCardInPopup = handleCardClick;
     this._cardSelector = cardSelector;
   }
 
@@ -65,16 +43,7 @@ class Card {
   }
 
   _handlePreviewPicture() {
-    // Студенты изучат способы описания взаимодействия между классами только в следующем спринте.
-    // Эту зависимость студенты будут передавать как хендлер в конструктор класса.
-    // Поэтому на данный момент они дублируют код из index.js в Card.js (Объявление переменных, функции)
-
-    imageElement.src = this._link;
-    imageElement.alt = `Изображение ${this._link}`;
-    imageCaption.textContent = this._text;
-
-    imageModalWindow.classList.add('popup_is-opened');
-    document.addEventListener('keyup', handleEscUp);
+    this._handleShowCardInPopup(this._link, this._text);
   }
 
   getView() {
