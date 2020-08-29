d7137687600809ec82a4ea1e34900cc4d1354c01
Batyrbek Aigaliyev <www.bat.94@gmail.com>
2020-08-30 00:47:54 +0500

    Добавил класс PopupWithForm

---
+++ src/scripts/PopupWithForm.js
@@ -0,0 +1,36 @@
+import Popup from './Popup';
+
+class PopuWithForm extends Popup {
+  constructor(selector, submitHandler) {
+    super(selector);
+    this._container = document.querySelector(selector);
+    this._form = this._container.querySelector('form');
+    this._submitHandler = null;
+
+    this.setEventListener(submitHandler);
+  }
+
+  _getInputValues() {
+    return this._form.elements;
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+
+  setEventListener(submitHandler, closeHandler = null) {
+    super.setEventListener(closeHandler);
+
+    if(typeof submitHandler !== 'function') return;
+
+    this._container.removeEventListener('submit', this._submitHandler);
+    this._submitHandler = (e) => {
+      e.preventDefault();
+      submitHandler(this._getInputValues());
+    }
+    this._container.addEventListener('submit', this._submitHandler);
+  }
+}
+
+export default PopuWithForm;
